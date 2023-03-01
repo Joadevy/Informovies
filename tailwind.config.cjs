b6e170f7823bd741744a32928fc0eaea6aa496fc
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-03-01 00:53:10 -0300

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        red: "#FC4747",
+        "dark-blue": "#10141E",
+        "grayish-blue": "rgb(90, 105, 143)",
+        "semi-dark-blue": "#161D2F",
+        "pure-white": "#FFFFFF",
+      },
+    },
+  },
+  plugins: [],
+};
