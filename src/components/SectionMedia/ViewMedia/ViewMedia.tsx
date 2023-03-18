27a4e7227e8d97d18fd5afc50c872157edb60ce2
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2023-03-18 20:40:42 -0300

    refactor: remove unnecesary LiMediaType component

--- src/components/SectionMedia/ViewMedia/ViewMedia.tsx
+++ src/components/SectionMedia/ViewMedia/ViewMedia.tsx
@@ -1,11 +1,10 @@
-import { getImageURL, getYear } from "@/utils/helpers";
+import { getImageURL } from "@/utils/helpers";
 import React from "react";
 import Image from "next/image";
 
 import Link from "next/link";
-import { IMedia, Movie, TVSeries } from "@/utils/types";
+import { IMedia } from "@/utils/types";
 import ButtonBookmark from "@/components/Buttons/ButtonBookmark";
-import LiMediaType from "./LiMediaType";
 import LiReleaseDate from "./LiReleaseDate";
 
 type props = {
@@ -13,10 +12,18 @@ type props = {
   sizeImages?: number;
 };
 
+const getMediaType = (media: IMedia) => {
+  if (!media.media_type) {
+    return media.first_air_date ? "tv" : "movie";
+  }
+  return media.media_type;
+};
+
 const ViewMedia = ({ media, sizeImages = 200 }: props) => {
+  const mediaType = getMediaType(media);
   return (
-    <div
-      // href={`/${media.id}`}
+    <Link
+      href={`/${mediaType}:${media.id}`}
       className={`relative rounded-xl shadow-xl flex-grow flex flex-col flex-wrap lg:bg-transparent group lg:perspective`}
     >
       <div className="lg:preserve-3d lg:group-hover:hover:my-rotate-y-180 lg:duration-1000">
@@ -35,7 +42,7 @@ const ViewMedia = ({ media, sizeImages = 200 }: props) => {
         </div>
         <footer className="flex flex-col lg:items-center lg:px-2 pt-2 lg:pt-10 lg:my-rotate-y-180 lg:absolute lg:top-0 lg:rounded-xl lg:backface-hidden lg:h-full lg:border lg:border-grayish-blue lg:w-full lg:bg-semi-dark-blue lg:overflow-hidden">
           <ul className=" text-xs flex gap-2 text-grayish-blue">
-            <LiMediaType media={media} />
+            <li>{mediaType === "tv" ? "Tv Series" : "Movie"}</li>
             <LiReleaseDate media={media} />
             <li>
               {"★".repeat(Math.round(media.vote_average / 2)).padEnd(5, "☆")}
@@ -45,7 +52,7 @@ const ViewMedia = ({ media, sizeImages = 200 }: props) => {
           <ButtonBookmark media={media} />
         </footer>
       </div>
-    </div>
+    </Link>
   );
 };
 
