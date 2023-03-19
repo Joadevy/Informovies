9f597b02b4c85f15cf75bc389148571e6bd96657
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-03-19 13:07:46 -0300

    refactor: use useMediaQuery instead of accesing window object

--- src/components/Icons/IconNavBookmarks.tsx
+++ src/components/Icons/IconNavBookmarks.tsx
@@ -1,8 +1,11 @@
 "use client";
 import { usePathname } from "next/navigation";
+import { useMediaQuery } from "@react-hook/media-query";
 
 const IconNavBookmarks = () => {
+  const isMobile = useMediaQuery("(max-width: 1024px)");
   const path = usePathname();
+
   return (
     <svg
       className={
@@ -10,7 +13,7 @@ const IconNavBookmarks = () => {
           ? "fill-white transition-colors"
           : "fill-grayish-blue hover:fill-red "
       }
-      viewBox={window.innerWidth >= 1024 ? "-2 0 20 20" : "0 0 20 20"}
+      viewBox={!isMobile ? "-2 0 20 20" : "0 0 20 20"}
       xmlns="http://www.w3.org/2000/svg"
     >
       <path d="M15.387 0c.202 0 .396.04.581.119.291.115.522.295.694.542.172.247.258.52.258.82v17.038c0 .3-.086.573-.258.82a1.49 1.49 0 0 1-.694.542 1.49 1.49 0 0 1-.581.106c-.423 0-.79-.141-1.098-.423L8.46 13.959l-5.83 5.605c-.317.29-.682.436-1.097.436-.202 0-.396-.04-.581-.119a1.49 1.49 0 0 1-.694-.542A1.402 1.402 0 0 1 0 18.52V1.481c0-.3.086-.573.258-.82A1.49 1.49 0 0 1 .952.119C1.137.039 1.33 0 1.533 0h13.854Z" />
