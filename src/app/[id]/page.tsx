aa642bfe91432399d445ed11eea70a45c95ce027
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-03-26 22:41:01 -0300

    feat: add genres to the content details

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -38,7 +38,7 @@ export default async function Media({ params }: Props) {
   const idMedia = getId(params.id);
   const Details = await getData(`${typeMedia}/${idMedia}`);
   return (
-    <main className="text-white flex gap-10 w-full min-h-screen">
+    <main className="text-white flex gap-10 w-full min-h-screen lg:pr-10">
       <Suspense fallback={<Loading />}>
         <div className="self-center h-[400px] w-[250px] lg:h-[500px] lg:w-[350px] relative">
           <Image
@@ -51,16 +51,29 @@ export default async function Media({ params }: Props) {
         </div>
       </Suspense>
 
-      <article className="w-3/4 flex flex-col gap-2 mt-[11vh]">
-        <h1 className="text-4xl font-bold">
-          {"name" in Details ? Details.name : Details.title}
-        </h1>
-        <p className="text-xl">
-          <span className="text-2xl">{getStars(Details.vote_average)}</span>{" "}
-          {`(${(Details.vote_average / 2).toFixed(2)})`}
-        </p>
+      <article className="w-7/12 flex flex-col gap-4 mt-[11vh]">
+        <header className="flex flex-col gap-2">
+          <h1 className="text-4xl font-bold">
+            {"name" in Details ? Details.name : Details.title}
+          </h1>
+          <div className="flex gap-5 items-center">
+            <ul className="flex gap-2">
+              {Details.genres.map((genre) => (
+                <li
+                  className="border border-grayish-blue text-grayish-blue py-1 px-2 rounded-lg bg-semi-dark-blue"
+                  key={genre.id}
+                >
+                  {genre.name}
+                </li>
+              ))}
+            </ul>
+            <p className="text-xl">
+              <span className="text-2xl">{getStars(Details.vote_average)}</span>{" "}
+              {`(${(Details.vote_average / 2).toFixed(2)})`}
+            </p>
+          </div>
+        </header>
         <p className="text-white-smoke">{Details.overview}</p>
-
         <a
           className="text-white-smoke rounded-xl p-2 border bg-red mt-5 max-w-fit"
           href={Details.homepage}
