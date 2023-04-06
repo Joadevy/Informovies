b3faedbcec44261585c21de91e2106b371379474
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-04-06 15:50:47 -0300

    refactor: add optional desescturing response

--- lib/getData.ts
+++ lib/getData.ts
@@ -1,10 +1,17 @@
 import { getApiURL } from "@/utils/helpers";
 
-const getData = async <T>(path: string, optional?: string): Promise<T[]> => {
+const getData = async <T>(
+  path: string,
+  optional?: string,
+  property?: string
+): Promise<T[]> => {
   const response = await fetch(getApiURL(path, optional), {});
   if (!response.ok) throw new Error(`Error while fetching ${path}`);
-
-  const { results } = await response.json();
+  if (!property) {
+    const { results } = await response.json();
+    return results;
+  }
+  const { [property]: results } = await response.json();
   return results;
 };
 
