04ce9107a15ee1a0c696608a17556b32fd36ff8c
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-04-30 20:35:09 -0300

    fix: mobile loader img

--- src/app/[id]/loading.tsx
+++ src/app/[id]/loading.tsx
@@ -3,7 +3,7 @@ import ClientLoader from "../../components/Loaders/ClientLoader";
 export default function Loading() {
   return (
     <div className="flex w-full min-h-screen">
-      <div className="flex items-center justify-center self-center h-[400px] w-[250px] lg:h-[500px] lg:w-[350px] relative border border-grayish-blue rounded-xl">
+      <div className="flex items-center justify-center lg:self-center h-[30vh] w-full sm:w-[450px] lg:h-[500px] lg:w-[350px] relative border border-grayish-blue rounded-xl">
         <ClientLoader />
       </div>
     </div>
