a2d21831fc1af71c38e15e9fb34bf418b4588a53
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-05-08 02:25:44 -0300

    feat: add functionality select genres in movies

--- src/components/FilterBars/GenreBar/GenreBarClient.tsx
+++ src/components/FilterBars/GenreBar/GenreBarClient.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { Genre } from "./GenreBar";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 type Props = {
   genres: Genre[];
@@ -10,23 +11,40 @@ type Props = {
 
 const GenreBarClient = ({ genres }: Props) => {
   const router = usePathname();
+  const [showOptions, toggleShowOptions] = useState(false);
+
   return (
     <>
-      {genres.map((genre) => (
-        // <option
-        //   key={genre.id}
-        //   value={`/${genre.name}`}
-        //   className="relative border bg-red"
-        // >
-        <Link
-          key={genre.id}
-          className="border text-white"
-          href={`${router}/${genre.name}`}
-        >
-          {genre.name}
-        </Link>
-        // {/* </option> */}
-      ))}
+      <input
+        className="w-full pr-10 pl-2 py-3 rounded-lg bg-semi-dark-blue border border-grayish-blue text-white-dust outline-none focus:border-white-dust hover:cursor-pointer hover:opacity-70 transition-opacity"
+        type="button"
+        onClick={() => toggleShowOptions(!showOptions)}
+        value="Genre"
+      />
+      <ul
+        className={
+          showOptions
+            ? `absolute flex flex-col gap-2 top-0 left-0 mt-12 w-[150px] bg-semi-dark-blue border border-white-dust shadow-lg rounded-lg p-2 transition-all duration-500 z-50 visible opacity-100 overflow-y-scroll max-h-72`
+            : "invisible opacity-0"
+        }
+      >
+        {showOptions
+          ? genres.map((genre) => (
+              <li
+                key={genre.id}
+                className="w-fit hover:opacity-70 transition-opacity"
+              >
+                <Link
+                  key={genre.id}
+                  className=""
+                  href={`${router}/${genre.name}`}
+                >
+                  <p className="w-full text-white-smoke">{genre.name}</p>
+                </Link>
+              </li>
+            ))
+          : null}
+      </ul>
     </>
   );
 };
