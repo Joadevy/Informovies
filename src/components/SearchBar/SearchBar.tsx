a2543ff44d923fbd4095da727ec67a13285d84cd
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-05-13 17:52:36 -0300

    feat: add close searchbar when clicks outside the input

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -2,12 +2,14 @@
 
 import { getApiURL } from "@/utils/helpers";
 import { MovieResult, TVResult } from "@/utils/types";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState, useCallback } from "react";
 import SearchResult from "./SearchResult";
 import Image from "next/image";
 import searchImg from "../../../public/assets/lupa.png";
 
 export const SearchBar = () => {
+  const refContainer = useRef<HTMLDivElement>(null);
+  const [close, toggleClose] = useState(false);
   const [results, setResults] = useState<(MovieResult | TVResult)[]>([]);
   const [searchQuery, setSearchQuery] = useState("");
 
@@ -15,8 +17,19 @@ export const SearchBar = () => {
     handleSearch();
   }, [searchQuery]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  const handleSearch = async () => {
-    if (searchQuery) {
+  useEffect(() => {
+    const handler = (e: MouseEvent) => {
+      if (refContainer.current?.contains(e.target as HTMLElement)) return;
+      toggleClose(true);
+    };
+
+    document.addEventListener("click", handler);
+
+    return () => document.removeEventListener("click", handler);
+  }, []);
+
+  const handleSearch = useCallback(async () => {
+    if (searchQuery.length > 1) {
       const response = await fetch(
         getApiURL("search/multi", `&query=${searchQuery}`)
       );
@@ -27,15 +40,18 @@ export const SearchBar = () => {
           .filter((result: MovieResult | TVResult) => "poster_path" in result) // Get only the movie/tv not the cast or crew
       );
     }
-  };
+  }, [searchQuery]);
 
   return (
     <>
-      <div className="relative">
+      <div className="relative" ref={refContainer}>
         <input
           className="w-full pl-8 p-3 rounded-lg bg-semi-dark-blue border border-grayish-blue text-white-smoke outline-none focus:border-white-dust"
           type="text"
           onChange={(e) => setSearchQuery(e.target.value)}
+          onClick={() => {
+            if (close) toggleClose(false);
+          }}
           placeholder="Search for any movies or tv shows..."
         />
         <div className="w-5 h-5 absolute top-3 left-2">
@@ -44,19 +60,16 @@ export const SearchBar = () => {
       </div>
       <ul
         className={
-          results.length > 0 && searchQuery
+          !close && results.length > 0 && searchQuery
             ? `absolute flex flex-col gap-2 top-0 left-0 mt-12 w-full bg-semi-dark-blue border border-white-dust shadow-lg rounded-lg p-1 transition-all duration-500 z-50 visible opacity-100 overflow-y-scroll max-h-72`
-            : "invisible opacity-0"
+            : "invisible"
         }
       >
-        {
-          results.length > 0 && searchQuery
-            ? results.map((result) => (
-                <SearchResult key={result.id} result={result} />
-              ))
-            : null
-          //   <li className="text-white-smoke">No results found</li>
-        }
+        {results.length > 0 && searchQuery && !close
+          ? results.map((result) => (
+              <SearchResult key={result.id} result={result} />
+            ))
+          : null}
       </ul>
     </>
   );
