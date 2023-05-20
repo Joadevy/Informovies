a6a5250383c8f69241a58fc2bb9902f64b2c5faa
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-05-20 17:44:03 -0300

    feat: add button to next series page

--- src/app/series/[genre]/page.tsx
+++ src/app/series/[genre]/page.tsx
@@ -1,6 +1,11 @@
 import SectionMedia from "@/components/SectionMedia";
 import SwipeSection from "@/components/SwipeSection";
-import { decodeURL, getIdGenreByName } from "@/utils/helpers";
+import {
+  decodeURL,
+  getGenreNameFromURL,
+  getGenrePageFromURL,
+  getIdGenreByName,
+} from "@/utils/helpers";
 import { Genre } from "@/utils/types";
 import getData from "lib/getData";
 import Link from "next/link";
@@ -12,9 +17,14 @@ type Props = {
 };
 
 export default async function Media({ params }: Props) {
-  const genreName = decodeURL(params.genre).toString();
+  const decodedURL = decodeURL(params.genre).toString();
+  const genreName = getGenreNameFromURL(decodedURL);
+  const genrePage = Number(getGenrePageFromURL(decodedURL));
+
+  console.log({ genrePage });
+
   const idGenres = await getData<Genre>("genre/tv/list", "", "genres");
-  const idGenre = getIdGenreByName(genreName, idGenres);
+  const idGenre = getIdGenreByName(genreName!, idGenres);
   return (
     <>
       <header className="py-4 flex gap-1 items-center lg:-ml-7 fixed z-20 bg-dark-blue w-full top-0">
@@ -22,7 +32,7 @@ export default async function Media({ params }: Props) {
           <h1 className="text-xl text-white-dust">{"Tv series"}</h1>
         </Link>
         <span className="text-xl text-white-dust">{">"}</span>
-        <span className="text-4xl text-white">{decodeURL(genreName)}</span>
+        <span className="text-4xl text-white">{genreName}</span>
       </header>
       <main className="text-white mt-16 flex flex-col gap-4">
         {/* @ts-expect-error Server Component */}
@@ -35,17 +45,22 @@ export default async function Media({ params }: Props) {
           showMediaType={false}
         />
 
-        {/* This will be a paginated component starting in 2 and user can navigate and update the request by incrementing &page=X+1 */}
         {/* @ts-expect-error Server Component */}
         <SectionMedia
           title="All we have for you"
           url={{
             path: "discover/tv",
-            optional: `&include_adult=false&with_genres=${idGenre}&sort_by=vote_count.desc&page=2`,
+            optional: `&include_adult=false&with_genres=${idGenre}&sort_by=vote_count.desc&page=${
+              genrePage + 1
+            }`,
           }}
           sizeImages={300}
           showMediaType={false}
         />
+
+        <Link href={`/series/genre=${genreName}&page=${Number(genrePage) + 1}`}>
+          NEXT PAGE
+        </Link>
       </main>
     </>
   );
