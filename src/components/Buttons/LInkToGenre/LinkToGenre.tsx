dd082474449810444b55c805355d5a77dd6a0f3c
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-05-20 18:51:57 -0300

    fix: update href in LinkToGenre buttons

--- src/components/Buttons/LInkToGenre/LinkToGenre.tsx
+++ src/components/Buttons/LInkToGenre/LinkToGenre.tsx
@@ -9,7 +9,7 @@ type Props = {
 const LinkToGenre = ({ genre: nameGenre, mediatype }: Props) => {
   return (
     <li className="border border-grayish-blue text-grayish-blue rounded-lg bg-semi-dark-blue hover:opacity-70 transition-opacity">
-      <Link className="" href={`${mediatype}/${nameGenre}`}>
+      <Link className="" href={`${mediatype}/genre=${nameGenre}&page=1`}>
         <p className="px-2 py-1">{nameGenre}</p>
       </Link>
     </li>
