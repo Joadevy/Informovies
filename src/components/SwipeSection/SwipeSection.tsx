b77cff166a7be9ad9186719a63559543c8928a69
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-06-03 21:21:05 -0300

    chore: add optional fetch to mix with the first one

--- src/components/SwipeSection/SwipeSection.tsx
+++ src/components/SwipeSection/SwipeSection.tsx
@@ -1,6 +1,7 @@
 import getData from "lib/getData";
 import SwipeSlider from "./SwipeSlider";
 import { MovieDetails, TvDetails } from "@/utils/types";
+import { shuffleArray } from "@/utils/helpers";
 
 type Props = {
   title: string;
@@ -8,15 +9,33 @@ type Props = {
     path: string;
     optional?: string;
   };
-  sizeImages?: number;
   showMediaType: boolean;
+  sizeImages?: number;
+  url2?: {
+    path: string;
+    optional?: string;
+  };
 };
 
-export const SwipeSection = async ({ title, url, showMediaType }: Props) => {
-  const media = await getData<TvDetails | MovieDetails>(
+export const SwipeSection = async ({
+  title,
+  url,
+  showMediaType,
+  url2,
+}: Props) => {
+  let media = await getData<TvDetails | MovieDetails>(
     url.path,
     url.optional ?? ""
   );
+
+  if (url2) {
+    const media2 = await getData<TvDetails | MovieDetails>(
+      url2.path,
+      url2.optional ?? ""
+    );
+    // console.log(media2);
+    media = shuffleArray(media.concat(media2));
+  }
 
   if (media.length === 0) return null;
 
