366631681974d14ae0bf5f90be93b4b9787fab12
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-06-21 15:24:36 -0300

    chore: add accessibility label

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -34,7 +34,7 @@ const Nav = () => {
   return (
     <nav className="flex lg:flex-col lg:gap-10 lg:h-[85vh] lg:max-w-min items-center justify-between lg:justify-between lg:rounded-lg p-5 lg:p-4 bg-semi-dark-blue fixed z-50 top-0 right-0 left-0 sm:left-1/2 sm:top-[2vh] sm:-translate-x-1/2 lg:right-10 lg:top-1/2 lg:left-[1vw] lg:-translate-y-1/2 lg:-translate-x-0">
       <div className="w-7 h-7 relative">
-        <Link href="/">
+        <Link href="/" aria-label="Home page">
           <Image src={icon} alt="" fill sizes="10vw" priority></Image>
         </Link>
       </div>
