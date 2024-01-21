2fea8cd81350ae90aad0bf51be47bbf7b344bf42
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2024-01-21 15:47:35 -0300

    style: add some text when no bookmarks

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -5,6 +5,8 @@ import { authOptions } from "@/utils/authOptions";
 import ViewBookmark from "@/components/SectionMedia/ViewBookmark/ViewBookmark";
 import { Bookmark } from "@/utils/types";
 import SearchBar from "@/components/SearchBar";
+import Link from "next/link";
+import { unstable_noStore } from "next/cache";
 
 export const metadata: Metadata = {
   title: "Bookmarks —Informovies",
@@ -18,6 +20,7 @@ export const fetchCache = "force-no-store";
 export default async function Bookmarks() {
   const session = await getServerSession(authOptions);
 
+  unstable_noStore();
   const resultSet = await db.execute(
     `SELECT * FROM Bookmark JOIN typeMedia on (Bookmark.typeMediaId = typeMedia.id)  JOIN User on (Bookmark.userId = User.id)  WHERE Bookmark.active AND email = '${session?.user?.email}'`
   );
@@ -35,15 +38,35 @@ export default async function Bookmarks() {
       </div>
 
       <h2 className="text-2xl m-2">Bookmarks</h2>
-      <div className="grid grid-cols-mobile lg:grid-cols-desktop gap-4 p-3">
-        {userBookmarks.map((media) => (
-          <ViewBookmark
-            key={media.mediaId}
-            media={media}
-            showDetails={true}
-          ></ViewBookmark>
-        ))}
-      </div>
+      {userBookmarks.length > 0 ? (
+        <div className="grid grid-cols-mobile lg:grid-cols-desktop gap-4 p-3">
+          {userBookmarks.map((media) => (
+            <ViewBookmark
+              key={media.mediaId}
+              media={media}
+              showDetails={true}
+            ></ViewBookmark>
+          ))}
+        </div>
+      ) : (
+        <p className="italic text-slate-400">
+          You don&apos;t have any bookmarks yet, take a look at our{" "}
+          <Link
+            href="/movies"
+            className="underline hover:text-transparent hover:bg-clip-text bg-gradient-to-r hover:from-blue-600 hover:via-green-500 hover:to-indigo-400"
+          >
+            movies
+          </Link>{" "}
+          or{" "}
+          <Link
+            href="/series"
+            className="underline hover:text-transparent hover:bg-clip-text bg-gradient-to-r hover:from-blue-600 hover:via-green-500 hover:to-indigo-400"
+          >
+            tv shows
+          </Link>{" "}
+          . We guarantee you&apos;ll find something you like!
+        </p>
+      )}
     </main>
   );
 }
